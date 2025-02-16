a92337eb8349ffbd71d4d150659162c9b5c44766
Ivan Santamans <ivansantamans@gmail.com>
2025-02-16 15:59:39 -0300

    now showing actual posts info

--- frontend/src/components/UserPosts.tsx
+++ frontend/src/components/UserPosts.tsx
@@ -1,7 +1,52 @@
-const UserPosts = () => {
-  return (
-    <h1>UserPosts</h1>
-  )
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  content: string;
 }
 
-export default UserPosts
+const UserPosts = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:5000/posts", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error al obtener los posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>The News</h1>
+        <h2>Posts</h2>
+      </div>
+      <div>
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          ))
+        ) : (
+          <p>No hay posts disponibles.</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default UserPosts;
